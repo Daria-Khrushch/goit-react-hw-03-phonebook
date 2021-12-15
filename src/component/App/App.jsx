075bd988dc1b44b8817738e0e192a9ca76f9f005
Daria Khrushch <Daria140694@gmail.com>
2021-12-15 19:50:24 +0200

    localstorageKey adding

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -16,6 +16,25 @@ class App extends React.Component {
     filter: '',
   };
 
+  #localstorageKey = 'contacts';
+
+  componentDidMount() {
+    const contacts = localStorage.getItem(this.#localstorageKey);
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(
+        this.#localstorageKey,
+        JSON.stringify(this.state.contacts),
+      );
+    }
+  }
+
   addContact = contact => {
     const duplicateContact = this.state.contacts.some(
       item => item.name.toLowerCase() === contact.name.toLowerCase(),
@@ -44,22 +63,6 @@ class App extends React.Component {
     return this.state.contacts.filter(contact =>
       contact.name.toLowerCase().includes(this.state.filter.toLowerCase()),
     );
-  }
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(`App componentDidUp`);
-    if (this.state.contacts !== prevState.contacts) {
-      console.log(`new`);
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
   }
 
   render() {
